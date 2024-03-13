4a12e0b925269a253513081761c13a121b36ea1f
An <22520033@gm.uit.edu.vn>
2024-03-13 13:54:18 +0700

    update note

--- Wecode/BTH3/sol1.py
+++ Wecode/BTH3/sol1.py
@@ -1,12 +1,28 @@
 import sys
 
-n = int(sys.stdin.readline())
-a = ( list(reversed(list(map(int, sys.stdin.readline().strip().split())))))
-m = int(sys.stdin.readline())
-b = list(map(int, sys.stdin.readline().strip().split()))
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            if mid == len(arr) - 1 or arr[mid + 1] != target:
+                return mid
+            else:
+                left = mid + 1
+        elif arr[mid] < target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
-for i in b:
-    try:
-        print(len(a) - 1 - a.index(i))
-    except:
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))[::-1]
+m = int(sys.stdin.readline())
+b = list(map(int, sys.stdin.readline().split()))
+
+for target in b:
+    idx = binary_search(a, target)
+    if idx != -1:
+        print(n - 1 - idx)
+    else:
         print(-1)
